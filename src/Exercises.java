1fd5ab56c86b1ab2179d1673f23bcf23a68d753b
aravishankar <aryanravishankar02@gmail.com>
2020-03-09 14:18:41 -0400

    Commit 2

--- src/Exercises.java
+++ src/Exercises.java
@@ -18,14 +18,56 @@ public class Exercises {
   }
 
   public int findMe(ArrayList<String> list, String target) {
+    
+    if (list == null || list.size() == 0 || target == null) {
+      return -1;
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) == target) {
+        return i;
+      }
+    }
+
     return -1;
   }
 
   public int findMeFaster(ArrayList<Integer> list, int target) {
+    if (list == null || list.size() == 0) {
+      return -1;
+    }
+
+    boolean isFound = false;
+    int start = 0;
+    int end = list.size();
+
+    while (!isFound) {
+      int midpoint = (start+end)/2;
+
+      if (list.get(midpoint) == target) {
+        return midpoint;
+      } else if (list.get(midpoint) < target) {
+        end = midpoint;
+      } else if (list.get(midpoint) > target) {
+        start = midpoint;
+      }
+
+    }
+    
     return -1;
   }
 
   public int findMeFaster(String[] list, String target) {
+    if (list == null || list.length == 0 || target == null) {
+      return -1;
+    }
+
+    boolean isFound = false;
+    int start = 0;
+    int end = list.length;
+
+    
+    
     return -1;
   }
 
