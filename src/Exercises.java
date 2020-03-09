72a7c80d56d6fc5641733b52165dc3f9ed205791
aravishankar <aryanravishankar02@gmail.com>
2020-03-09 14:31:41 -0400

    Commit 5

--- src/Exercises.java
+++ src/Exercises.java
@@ -84,7 +84,37 @@ public class Exercises {
   }
 
   public int[] bubble(int[] list, boolean ascending) {
-    return null;
+    
+    if (list == null || list.length == 0 || ascending == null) {
+      return null;
+    }
+
+    int[] arr = list;
+
+    if (ascending) {
+      for (int i = 0; i < list.length -1; i++) {
+        for (int j = 0; j < list.length -1 -i; j++) {
+          if (arr[j] > arr[j+1]) 
+                { 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
+        }
+      }
+    } else if (!ascending) {
+      for (int i = list.length; i > 1; i--) {
+        for (int j = list.length; j > i - 1; j--) {
+          if (arr[j] < arr[j - 1]) {
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j + 1] = temp;
+          }
+        }
+      }
+    }
+    
+    return arr;
   }
 
   public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
